fe37306c5879aac02576f6d7bc6281ec6ab715e4
valentina-lamanna <62357337+valentina-lamanna@users.noreply.github.com>
2022-05-27 10:25:11 -0300

    freno por auto y neumático por auto

--- cargar_datos_3.sql
+++ cargar_datos_3.sql
@@ -82,4 +82,65 @@ WHERE M.TELE_MOTOR_NRO_SERIE IS NOT NULL AND M.TELE_CAJA_NRO_SERIE IS NOT NULL
                 CLOSE CURSOR_AUTOS
                 DEALLOCATE CURSOR_AUTOS
 
-         
+
+--FRENO POR AUTO
+--begin tran freno
+insert into COSMICOS.FRENO_POR_AUTO ( FRENO_NRO_SERIE , CODIGO_AUTO, ACTIVO)
+SELECT DISTINCT TELE_FRENO1_NRO_SERIE, a.CODIGO_AUTO, 0
+FROM gd_esquema.Maestra m
+JOIN COSMICOS.AUTO a ON m.AUTO_NUMERO = a.AUTO_NUMERO AND m.AUTO_MODELO = a.AUTO_MODELO
+WHERE TELE_FRENO1_NRO_SERIE IS NOT NULL
+UNION
+SELECT DISTINCT TELE_FRENO2_NRO_SERIE, a.CODIGO_AUTO, 0
+FROM gd_esquema.Maestra m
+JOIN COSMICOS.AUTO a ON m.AUTO_NUMERO = a.AUTO_NUMERO AND m.AUTO_MODELO = a.AUTO_MODELO
+WHERE TELE_FRENO2_NRO_SERIE IS NOT NULL
+UNION
+SELECT DISTINCT TELE_FRENO3_NRO_SERIE, a.CODIGO_AUTO, 0
+FROM gd_esquema.Maestra m
+JOIN COSMICOS.AUTO a ON m.AUTO_NUMERO = a.AUTO_NUMERO AND m.AUTO_MODELO = a.AUTO_MODELO
+WHERE TELE_FRENO3_NRO_SERIE IS NOT NULL
+UNION 
+SELECT DISTINCT TELE_FRENO4_NRO_SERIE, a.CODIGO_AUTO, 0
+FROM gd_esquema.Maestra m
+JOIN COSMICOS.AUTO a ON m.AUTO_NUMERO = a.AUTO_NUMERO AND m.AUTO_MODELO = a.AUTO_MODELO
+WHERE TELE_FRENO4_NRO_SERIE IS NOT NULL
+ORDER BY 2
+
+--seteamos los activos en 1
+
+DECLARE @auto_num int, @auto_modelo nvarchar(255), @cod int
+DECLARE CURSOR_AUTOS CURSOR FOR 
+	SELECT AUTO_MODELO, AUTO_NUMERO, CODIGO_AUTO FROM COSMICOS.AUTO
+    OPEN CURSOR_AUTOS
+    FETCH NEXT FROM CURSOR_AUTOS INTO @auto_modelo, @auto_num, @cod
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+		DECLARE @fre1 nvarchar(255), @fre2 nvarchar(255), @fre3 nvarchar(255), @fre4 nvarchar(255)
+
+		SELECT DISTINCT @fre1 = TELE_FRENO1_NRO_SERIE, 
+                        @fre2 = TELE_FRENO2_NRO_SERIE, 
+                        @fre3 = TELE_FRENO3_NRO_SERIE, 
+                        @fre4 = TELE_FRENO4_NRO_SERIE 
+                 FROM [gd_esquema].[Maestra] m
+                 WHERE AUTO_MODELO = @auto_modelo AND AUTO_NUMERO = @auto_num 
+                        AND  TELE_FRENO1_NRO_SERIE IS NOT NULL
+                        AND  TELE_FRENO2_NRO_SERIE IS NOT NULL
+                        AND  TELE_FRENO3_NRO_SERIE IS NOT NULL
+                        AND  TELE_FRENO4_NRO_SERIE IS NOT NULL
+
+                      -- este "print" se ve en la tab de Messages de SQL Server
+                      PRINT 'PARA EL AUTO ' + CAST(@cod as varchar) + ' LOS NUEMATICOS ACTIVOS SON ' + 
+                        CAST(@fre1 as varchar) + ', ' +  CAST(@fre2 as varchar) + ', ' + CAST(@fre3 as varchar) + ', ' + CAST(@fre4 as varchar)
+
+		UPDATE COSMICOS.FRENO_POR_AUTO
+               SET ACTIVO = 1
+               WHERE FRENO_NRO_SERIE in (@fre1, @fre2, @fre3, @fre4) AND CODIGO_AUTO = @cod
+
+        FETCH NEXT FROM CURSOR_AUTOS INTO @auto_modelo, @auto_num, @cod
+	END
+    CLOSE CURSOR_AUTOS
+    DEALLOCATE CURSOR_AUTOS
+    
+    
+--BANDERA Y TIPO DE INCIDENTE EN EL SCRIPT 2
