c3fd4e7e814690e7b940076c15e43eeee3c3a173
valentina-lamanna <62357337+valentina-lamanna@users.noreply.github.com>
2022-06-15 23:14:25 -0300

    Create Valen.sql

---
+++ Valen.sql
@@ -0,0 +1,5 @@
+--Mejor tiempo de vuelta de cada escudería por circuito por año.
+--El mejor tiempo está dado por el mínimo tiempo en que un auto logra realizar una vuelta de un circuito
+
+
+--Los 3 circuitos donde se consume mayor cantidad en tiempo de paradas en boxes
