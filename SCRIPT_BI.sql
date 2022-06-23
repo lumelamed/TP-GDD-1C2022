39769203454743b1797f9ef350359160ff0e674e
valentina-lamanna <62357337+valentina-lamanna@users.noreply.github.com>
2022-06-23 20:50:57 -0300

    Update SCRIPT_BI.sql

--- SCRIPT_BI.sql
+++ SCRIPT_BI.sql
@@ -112,33 +112,61 @@ LEFT JOIN  [COSMICOS].[TIPO_SECTOR] T ON S.SECTOR_TIPO = T.SECTOR_TIPO
 --INSERT EN LOS HECHOS-----------------------------------------------------------------------------------------------------------------------------------------------
 --Q1
 --Desgaste promedio de cada componente de cada auto por vuelta por circuito. 
---Tener en cuenta que, para el c·lculo del desgaste de los neum·ticos, se  toma la diferencia de mm del mismo entre la mediciÛn inicial y final de cada vuelta.
---Lo mismo aplica para el desgaste de frenos. Para el c·lculo del desgaste del motor se toma en cuenta la perdida de potencia. 
+--Tener en cuenta que, para el c√°lculo del desgaste de los neum√°ticos, se  toma la diferencia de mm del mismo entre la medici√≥n inicial y final de cada vuelta.
+--Lo mismo aplica para el desgaste de frenos. Para el c√°lculo del desgaste del motor se toma en cuenta la perdida de potencia. 
 
 
 --Q2
---Mejor tiempo de vuelta de cada escuderÌa por circuito por aÒo. 
---El mejor tiempo est· dado por el mÌnimo tiempo en que un auto logra realizar una vuelta de un circuito. 
+--Mejor tiempo de vuelta de cada escuder√≠a por circuito por a√±o. 
+--El mejor tiempo est√° dado por el m√≠nimo tiempo en que un auto logra realizar una vuelta de un circuito. 
 
+--SIN CHEQUEAR!!!
+insert into COSMICOS.HechosPrincipal (Q2_MejorTiempoVuelta_XEscuderia_XCircuito_Xanio,Anio ,CODIGO_AUTO, CODIGO_ESCUDERIA, CIRCUITO_CODIGO) 
+select TELE_AUTO_TIEMPO_VUELTA , year(C.CARRERA_FECHA) as A√ëO_CARRERA , A.CODIGO_AUTO, E.CODIGO_ESCUDERIA, C.CIRCUITO_CODIGO
+from cosmicos.TELEMETRIA T
+join COSMICOS.AUTO_POR_CARRERA AC on T.CODIGO_AUTO_POR_CARRERA = AC.CODIGO_AUTO_POR_CARRERA
+join COSMICOS.AUTO A on A.CODIGO_AUTO = AC.CODIGO_AUTO
+join COSMICOS.ESCUDERIA E on E.CODIGO_ESCUDERIA = A.CODIGO_ESCUDERIA
+join COSMICOS.CARRERA C on C.CODIGO_CARRERA = AC.CODIGO_CARRERA
+where TELE_AUTO_TIEMPO_VUELTA <> 0  and  TELE_AUTO_TIEMPO_VUELTA = (
+		select top 1 TELE_AUTO_TIEMPO_VUELTA from COSMICOS.TELEMETRIA T1
+		join COSMICOS.CARRERA C1 on C1.CODIGO_CARRERA = T1.CODIGO_CARRERA
+		join COSMICOS.AUTO_POR_CARRERA AC1 on T1.CODIGO_AUTO_POR_CARRERA = AC1.CODIGO_AUTO_POR_CARRERA
+		join COSMICOS.AUTO A1 on A1.CODIGO_AUTO = AC1.CODIGO_AUTO
+		join COSMICOS.ESCUDERIA E1 on E1.CODIGO_ESCUDERIA = A1.CODIGO_ESCUDERIA and E1.CODIGO_ESCUDERIA = E.CODIGO_ESCUDERIA
+		where  year(C.CARRERA_FECHA) = year(C1.CARRERA_FECHA) and   TELE_AUTO_TIEMPO_VUELTA <> 0 and C.CIRCUITO_CODIGO = C1.CIRCUITO_CODIGO
+		group by  t1.TELE_AUTO_TIEMPO_VUELTA ,c1.CODIGO_CARRERA
+		order by  t1.TELE_AUTO_TIEMPO_VUELTA 
+		)	
+order by CODIGO_ESCUDERIA,  A√ëO_CARRERA,TELE_AUTO_TIEMPO_VUELTA 
 
 --Q3
 --Los 3 de circuitos con mayor consumo de combustible promedio. 
 
 --Q4
---M·xima velocidad alcanzada por cada auto en cada tipo de sector de cada circuito. 
+--M√°xima velocidad alcanzada por cada auto en cada tipo de sector de cada circuito. 
 
 
 --Q5
---Tiempo promedio que tardÛ cada escuderÌa en las paradas por cuatrimestre. 
+--Tiempo promedio que tard√≥ cada escuder√≠a en las paradas por cuatrimestre. 
 
 --Q6
---Cantidad de paradas por circuito por escuderÌa por aÒo. 
+--Cantidad de paradas por circuito por escuder√≠a por a√±o. 
 
 --Q7
 --Los 3 circuitos donde se consume mayor cantidad en tiempo de paradas en boxes. 
 
+--SIN CHEQUEAR!!
+
+insert into COSMICOS.HechosPrincipal (CIRCUITO_CODIGO,  Q7_Tiempo_XParada_XCircuito)
+select  c.CIRCUITO_CODIGO, sum(p.PARADA_BOX_TIEMPO)
+from cosmicos.PARADA_BOX p 
+join  cosmicos.CARRERA c on c.CODIGO_CARRERA =p.CODIGO_CARRERA
+group by c.CIRCUITO_CODIGO  
+order by sum(p.PARADA_BOX_TIEMPO) desc
+
 --Q8
---Los 3 circuitos m·s peligrosos del aÒo, en funciÛn mayor cantidad de incidentes. 
+--Los 3 circuitos m√°s peligrosos del a√±o, en funci√≥n mayor cantidad de incidentes. 
 
 --Q9
---Promedio de incidentes que presenta cada escuderÌa por aÒo en los distintos tipo de sectores. 
+--Promedio de incidentes que presenta cada escuder√≠a por a√±o en los distintos tipo de sectores. 
